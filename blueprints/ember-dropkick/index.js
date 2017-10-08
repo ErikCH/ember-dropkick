93db82bde7cfa3d7360d3cd253f97dcd929d46a5
Erik Hanchett <icystorm@gmail.com>
2017-10-08 16:46:11 -0700

    Fixed Package.json files

--- blueprints/ember-dropkick/index.js
+++ blueprints/ember-dropkick/index.js
@@ -4,6 +4,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('dropkick#2.1.9');
+    return this.addBowerPackageToProject('dropkick','2.1.9');
   }
 };
